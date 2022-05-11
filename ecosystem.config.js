9c47b832ba95f4ed735cde49424d411cde8c65ab
Landalf YAO <892538405@qq.com>
2022-05-11 11:53:09 +0800

    fix: 修复bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "ddrunv2-server",
+      name: "ddrunv2-free-server",
       script: "bootstrap.js",
       cwd: "./server/",
       // 是否启用监控模式，默认是false。如果设置成true，当应用程序变动时，pm2会自动重载。这里也可以设置你要监控的文件。
